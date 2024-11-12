1afd4475a11a53311232c3e6ba56a41daa69277a
Andreajguerra <andreaguerra0507@gmail.com>
2024-11-11 20:27:33 -0400

    Create Andreajguerra.js
    ESTA ES MI RESOLUCIóN DEL EJERCICIO 00

---
+++ Roadmap/00 - SINTAXIS, VARIABLES, TIPOS DE DATOS Y HOLA MUNDO/javascript/Andreajguerra.js
@@ -0,0 +1,30 @@
+//https://developer.mozilla.org/es/docs/Web/JavaScript//
+
+/*COMENTARIO DE MÚLTIPLES LÍNEAS*/
+
+
+const NOMBRE_NOMBRE ="mi nueva variable" //Las constantes son como letreros que indican que algo no debe cambiar. Son muy útiles en programación porque hacen que nuestro código sea más fácil de entender y menos propenso a errores.//
+
+const EDAD_DE_JUAN = 7; // Este tipo de dato primitivo (Number)nos ayuda a representar números, tanto enteros como decímales//
+
+const NOMBRE = "Carlos"; const saludo= '¡Hola!'; // Este tipo de dato primitivo llamado string representa texto. Se escriben entre comillas simples o dobles.
+
+
+const NUMERO_1= true;
+const NUMERO_2= false; //Este tipo de dato primitivo llamado Boolean representa valores lógicos, verdadero (true) o falso (false)//
+
+const ZAPATO= 2;
+const TACÓN= null; // Este tipo de dato primitivo llamado Null nos ayuda a representar la ausencia internacional de un valor//
+
+var andreajguerra; // Este tipo de dato primitivo llamado Undefined representa una variable que ha sido declarada pero aún no ha sido asignada.//
+
+var number= 7;
+var string= "Hola, que tal?";
+var esEstudiante= True;
+var tieneHambre= false;// tipo de dato Booleano//
+var día1= 10;
+var día2= null;// Null ausencia de un valor//
+var andreajguerra; //Undefined//
+
+console.log("¡Hola, JavaScript!");
+
